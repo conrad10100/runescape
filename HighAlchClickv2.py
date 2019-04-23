ac64e65359cbcca54f1290227e454de1728775e5
Conrad Chiu <conrockz007@gmail.com>
2019-04-23 00:13:53 -0400

    issue with image recognition will continue to work on

--- HighAlchClickv2.py
+++ HighAlchClickv2.py
@@ -5,12 +5,14 @@ print(screenHeight)
 
 alchSpell = pyautogui.locateOnScreen('images\HighAlchSpell.png', grayscale=True)
 moveToSpellX, moveToSpellY = pyautogui.center(alchSpell)
+
+magicShieldBow = pyautogui.locateOnScreen('images\MagicShieldbow.png', grayscale=True)
+moveToShieldX, moveToShieldY = pyautogui.center(magicShieldBow)
 while pyautogui.FAILSAFE == True:
     for x in range(0, 27):
 
         pyautogui.click(moveToSpellX, moveToSpellY, duration=2)
         pyautogui.PAUSE=4
-        magicShieldBow = pyautogui.locateOnScreen('images\MagicShieldbow.png', grayscale=True)
-        moveToShieldX, moveToShieldY = pyautogui.center(magicShieldBow)
+
         pyautogui.click(moveToShieldX, moveToShieldY, duration=2)
         pyautogui.PAUSE = 4
