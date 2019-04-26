d89dd9a6ee7fc962162c584247f12e32fe1cf3c9
Conrad Chiu <conrockz007@gmail.com>
2019-04-25 23:46:23 -0400

    Update HighAlchClick.py

--- HighAlchClick.py
+++ HighAlchClick.py
@@ -20,22 +20,22 @@ def movement():
     xItem = itemX + random.randint(-3, 20)
     yItem = itemY + random.randint(-20, 0)
 
-    xOffCurve = (xSpell+xItem)/2
-    yOffCurve = (ySpell+yItem)/2
+    ##The values have to be off the line to cause the curve
+    xOffCurve = ((xSpell+xItem)/2)+random.randint(-30, 30)
+    yOffCurve = ((ySpell+yItem)/2)+random.randint(-30, 30)
 
-    pyautogui.mouseDown(xSpell, ySpell, duration=1.0 + random.random())
-    pyautogui.click()
-    pyautogui.PAUSE = .3 + random.random()
-    for t in range(0,1):
-        xSpellToItem = xOffCurve + (1 - t) * (1 - t) * (xSpell - xOffCurve) + t * t * (xItem - xOffCurve)
-        ySpellToItem = yOffCurve + (1 - t) * (1 - t) * (ySpell - yOffCurve) + t * t * (yItem - yOffCurve)
-        pyautogui.moveTo(xSpellToItem,ySpellToItem, duration=0, pause=None)
+    pyautogui.move(xSpell, ySpell, duration=1.5 + random.random())
+    pyautogui.click(xSpell, ySpell)
+    pyautogui.PAUSE = 0.3 + random.random()
+
+    for t in range(0,3):
+        xSpellToItem = (1-t/3)*(1-t/3)*xSpell+2*(1-t/3)*t/3*xOffCurve+t/3*t/3*xItem
+        ySpellToItem = (1-t/3)*(1-t/3)*ySpell+2*(1-t/3)*t/3*yOffCurve+t/3*t/3*yItem
+        pyautogui.move(xSpellToItem, ySpellToItem, duration=0.00, pause=None)
 
 
-    pyautogui.moveTo(xItem, yItem, duration=1.0 + random.random())
-    pyautogui.click()
-    pyautogui.PAUSE = .3 + random.random()
-
+    pyautogui.move(xItem, yItem, duration=1)
+    pyautogui.click(xItem, yItem)
 
 #Added random values to avoid bot detection
 while pyautogui.FAILSAFE == True:
