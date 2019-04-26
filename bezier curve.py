0936baafc25eb211ffac5f3ee555b8afd3c0f83c
Conrad Chiu <conrockz007@gmail.com>
2019-04-25 23:45:57 -0400

    Update bezier curve.py

--- bezier curve.py
+++ bezier curve.py
@@ -17,7 +17,7 @@ def movement():
     for t in range(0,10):
         xSpellToItem = (1-t/10)*(1-t/10)*x0+2*(1-t/10)*t/10*x1+t/10*t/10*x2
         ySpellToItem = (1-t/10)*(1-t/10)*y0+2*(1-t/10)*t/10*y1+t/10*t/10*y2
-        pyautogui.mouseDown(xSpellToItem,ySpellToItem, duration=0.0, pause=None, tween=0)
+        pyautogui.mouseDown(xSpellToItem,ySpellToItem, duration=0.0, pause=None)
 
 
     pyautogui.mouseDown()
